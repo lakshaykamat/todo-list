281137e84ba678429c77d5377b29a66e0aa1b31f
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-25 20:09:34 +0530

    new changes

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ function printList(){
     num++//num = num + 1
     styleContainer()
     h1Task.innerHTML =   `<h1 class="text-center text-4xl m-5">Your Tasks</h1>`
-    container.innerHTML += `<li class="m-5 py-2  text-lg"> ${num}. ${listItem[0]}</li>
+    container.innerHTML += 
+    `<div>
+    <li class="m-5 py-2  text-lg"> ${num}. ${listItem[0]}</li>
     <button id="del">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
@@ -34,7 +36,8 @@ function printList(){
         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
         </svg>
     </button>
-    <hr class="w-full h-[.2rem] bg-white">`
+    <hr class="w-full h-[.2rem] bg-white">
+    </div>`
 }
 function styleContainer(){
     container.style.border = "2px solid white"
