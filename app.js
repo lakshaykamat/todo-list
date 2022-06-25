871b0d079b6b33db4c87e2e989efebbe66b13abc
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-25 14:06:48 +0530

    magin padding is reduced on input

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let errorMsg = document.getElementById('error-msg')
 
 btn.addEventListener("click", function(){//Onclick event in button
     if(inputEl.value === ''){//if input filed is empty nothing happens
-        errorMsg.textContent = "Enter a task"//Show error msg
+        errorMsg.textContent = "Enter a task to add"//Show error msg
         inputEl.addEventListener("click",function(){//if user clicked on input filed error msg is gone
             errorMsg.textContent = ''
         })
