9e964bcde9c729092c6ea14c17cbb3c007b5cfce
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-26 20:25:29 +0530

    comments added

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const btn = document.getElementById('btn')
 const inputEl = document.getElementById('input-el');
 const container = document.getElementById('container')
-const h1Task = document.getElementById('Yourtasks')
+const h1Task = document.getElementById('Yourtasks-div')
 const errorMsg = document.getElementById('error-msg')
 const del = document.getElementById('del') 
 const done = document.getElementById('done')
-let listItem = []
+let listItem = []//array for storing list items
 
-//Enter key function 
+//Adding event listner to start adding items
+btn.addEventListener("click",startOperation)
+//Enter key function [COPIED FUNCTION]
 inputEl.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
     btn.click();
   }});
-//Button event listner
-btn.addEventListener("click", function(){//Onclick event in button
+
+//Onclick event in 'add to list' button
+function startOperation(){
     if(inputEl.value === ''){//if input filed is empty nothing happens
         errorMsg.textContent = "You hav'nt enter a task"//Show error msg
         inputEl.addEventListener("click",function(){//if user clicked on input filed error msg is gone
@@ -23,26 +26,27 @@ btn.addEventListener("click", function(){//Onclick event in button
     }
     else{//otherwise excute
         listItem.unshift(inputEl.value)//pushing input value to array
-        printList()
-        inputEl.value = ''
+        printList()//calling function
+        inputEl.value = ''//Removing the text text from input field
     }
-})
-//Starts to call fuction
-function printList(){
-    styleContainer()
-    h1Task.innerHTML =   `<h1 class="text-center text-4xl m-5">Your Tasks</h1>`
-    printItemsofScreen()
 }
-//Style the unoreder list container
+//printList function
+function printList(){
+    styleContainer()//Calling StyleContainer() to style the <ul> tag
+    h1Task.innerHTML =   `<h1 class="text-center text-4xl m-5">Your Tasks</h1>`//Printing H1 tag in his parent div tag
+    printListItemsonScreen()//Printing the list items
+}
+//Style the unorederd list container
 function styleContainer(){
-    container.style.border = "3px solid #000"
-    container.style.borderRadius = "10px"
+    container.style.border = "3px solid #000"//adding border to container
+    container.style.borderRadius = "10px"//adding border radius to container
 }
 //Prints List to HTML
-function printItemsofScreen(){
-let containerFirstChild = container.firstElementChild
-    let divList = document.createElement('div')
-    divList.classList.add("sm:mx-8", "sm:my-4","my-2","mx-5")
+function printListItemsonScreen(){
+let containerFirstChild = container.firstElementChild //grabing the container(ul tag) first child
+    let divList = document.createElement('div')//creating a div tag
+    divList.classList.add("sm:mx-8", "sm:my-4","my-2","mx-5")//adding a class to  created div
+    //adding li tag and buttons to created div tag
     divList.innerHTML = `
     <li class="m-3 py-2  text-xl break-normal"> ${listItem[0]} </li> 
     <button id="del" class="mx-2">
@@ -56,9 +60,19 @@ let containerFirstChild = container.firstElementChild
     </svg>
 </button>
 <hr class="w-full h-[.2rem] bg-[#000] rounded">`
-    if(containerFirstChild == null){
-        container.appendChild(divList)
-    }else{
-        container.insertBefore(divList,containerFirstChild)
+    //if user add his new item to existing item list then it should at 1st place
+    if(containerFirstChild == null){//if container has not child in it 
+        container.appendChild(divList)//add the created div tag to it
+    }else{//has already a child in it
+        container.insertBefore(divList,containerFirstChild)//insert created div before the existing child
     }
 } 
+// function remove(e){
+//     const item = e.target
+//     // console.log(event.currentTarget)
+//     // console.log(item)
+//     if(item.classList == "del"){
+//         const todo = item.parentElement
+//         todo.remove()
+//     }
+// }
